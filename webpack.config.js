5f79c78ca225e439834ec620b94780a35e25c695
Arash-Seifi <arashseifi81@gmail.com>
2022-12-31 23:34:53 +0330

    Making the Bootstrap Guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,43 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    mode : 'development',
-    entry : "./src/index.js",
-    output : {
-        filename : "main.js",
-        path : path.resolve(__dirname,"dist")
-    }
-}
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
