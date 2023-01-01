1c1bf59695f123fca50df28135b758ca1bd00cc6
Arash-Seifi <arashseifi81@gmail.com>
2023-01-01 16:33:39 +0330

    Making the Tailwindcss guide

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,6 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+// Import all Tailwindcss
+import '../css/styles.css'
--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,10 @@ module.exports = {
             loader: 'sass-loader'
           }
         ]
+      },
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
       }
     ]
   }
